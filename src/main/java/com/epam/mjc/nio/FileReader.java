1c987f73b58b3e84a7a5c5936654f39347b2efa5
Mykty <mykty-baha@bk.ru>
2022-09-26 08:58:48 +0600

    done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,55 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = null;
+        String allStr = "", name = "", email = "";
+        int age = 0;
+        long phoneNumber = 0;
+
+        try(RandomAccessFile aFile = new RandomAccessFile("src/main/resources/Profile.txt", "r");
+            FileChannel inChannel = aFile.getChannel();) {
+
+            long fileSize = inChannel.size();
+            ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
+            inChannel.read(buffer);
+            buffer.flip();
+
+            for (int i = 0; i < fileSize; i++) {
+                char c = (char) buffer.get();
+                if(c == '\n'){
+                    if(allStr.contains("Name")){
+                        name = allStr.substring(allStr.indexOf(" ")+1);
+                        allStr = "";
+                    }
+                    else if(allStr.contains("Age")){
+                        age = Integer.parseInt(allStr.substring(allStr.indexOf(" ")+1));
+                        allStr = "";
+                    }
+                    else if(allStr.contains("Email")){
+                        email = allStr.substring(allStr.indexOf(" ")+1);
+                        allStr = "";
+                    }else if(allStr.contains("Phone")){
+                        phoneNumber = Long.parseLong(allStr.substring(allStr.indexOf(" ")+1));
+                        allStr = "";
+                    }
+                }else {
+                    allStr += c;
+                }
+            }
+            profile = new Profile(name, age, email,phoneNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
     }
 }
